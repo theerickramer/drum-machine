8a47487853e61cb84a6c36c974f76c44abccf396
Eric Kramer <theerickramer@gmail.com>
2018-02-16 17:16:13 -0500

    gridding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hi</h1>
+      <div className="drum-machine">
+        <div className="name">
+          <h1>808</h1>
+        </div>
+        <div className="controls">
+          <button className="button--stop" />
+          <button className="button--play" />
+          <div className="bpm">
+            <input className="bpm__input" value="128" id="bpm" />
+            <label className="bpm__label" htmlFor="bpm">
+              BPM
+            </label>
+          </div>
+          <select className="sequence">
+            <option>Sequence 1</option>
+            <option>Sequence 2</option>
+            <option>Sequence 3</option>
+          </select>
+        </div>
+        <div className="step-column-headers"></div>
+        <div className="instruments"></div>
+        <div className="steps"></div>
       </div>
     );
   }
