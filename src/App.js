b0dd5c53a929ac42b81f90fb6112f128274474f8
Eric Kramer <theerickramer@gmail.com>
2018-02-22 17:55:04 -0500

    App calculateInterval() passes test

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import './App.css';
 
 class App extends Component {
   state = {
-    steps: 16,
+    bpm: 128,
     instruments: [
       { name: 'kick', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-      { name: 'snare', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-      { name: 'open hat', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-      { name: 'closed hat', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-    ]
+      {
+        name: 'snare',
+        steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      },
+      {
+        name: 'open hat',
+        steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      },
+      {
+        name: 'closed hat',
+        steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      }
+    ],
+    steps: 16
   };
+  calculateInterval(bpm) {
+    return 60 / this.state.bpm * 4 / this.state.steps * 1000;
+  }
   render() {
-    return <div className="drum-machine">
-        <Header steps={this.state.steps} />
+    return (
+      <div className="drum-machine">
+        <Header bpm={this.state.bpm} steps={this.state.steps} />
         <ul className="instruments">
           {this.state.instruments.map((instrument, index) => (
             <li className="instrument" key={index}>
@@ -23,7 +37,8 @@ class App extends Component {
           ))}
         </ul>
         <div className="steps" />
-      </div>;
+      </div>
+    );
   }
 }
 
