c8458f6a0f447190933c9113f67559ecdcdfa8ab
Eric Kramer <theerickramer@gmail.com>
2018-02-22 13:53:58 -0500

    more instruments h4 margin taking care of centering for now

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   state = {
     steps: 16,
     instruments: [
-      { name: 'kick', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+      { name: 'kick', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+      { name: 'snare', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+      { name: 'open hat', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+      { name: 'closed hat', steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
     ]
   };
   render() {
@@ -14,7 +17,7 @@ class App extends Component {
       <div className="drum-machine">
         <Header steps={this.state.steps} />
         <ul className="instruments">
-          {this.state.instruments.map(instrument => <li>{instrument.name}</li>)}
+          {this.state.instruments.map(instrument => <li className="instrument"><h4 className="instrument__name">{instrument.name}</h4></li>)}
         </ul>
         <div className="steps" />
       </div>
