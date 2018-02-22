554b621a52035039eaac4e8dbeebdad32c914d0b
Eric Kramer <theerickramer@gmail.com>
2018-02-22 17:51:17 -0500

    calculateInterval() test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import StepHeaders from './StepHeaders';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe(`<App>`, () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it(`converts bpm tempo to step interval in milliseconds`, () => {
+    const bpm = 128;
+    const expected = 60 / bpm * 4 / 16 * 1000;
+    const actual = new App().calculateInterval(bpm);
+    expect(actual).toEqual(expected);
+  });
 });
