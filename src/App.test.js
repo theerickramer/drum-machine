db378dccc64843a010462fb4b0c4ac4ee23ae125
Eric Kramer <theerickramer@gmail.com>
2018-02-23 18:33:02 -0500

    step headers test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 import StepHeaders from './StepHeaders';
+
+configure({ adapter: new Adapter() });
 
 describe(`<App>`, () => {
   it('renders without crashing', () => {
@@ -15,5 +19,13 @@ describe(`<App>`, () => {
     const expected = 60 / bpm * 4 / 16 * 1000;
     const actual = new App().calculateInterval(bpm);
     expect(actual).toEqual(expected);
+  });
+});
+
+describe(`<StepHeaders>`, () => {
+  it(`renders <li> elements for the number of steps in App state`, () => {
+    const app = mount(<App />);
+    const steps = app.state('steps');
+    expect(app.find('.step-headers').children().length).toEqual(steps);
   });
 });
